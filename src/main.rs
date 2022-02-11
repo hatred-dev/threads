042ab629a46f3bf1ecc0cda77225ef86b35b002a
Edward Zelis <zeledvards@gmail.com>
2022-02-11 19:40:31 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
-use std::sync::mpsc;
 use std::thread;
 
 //our threshold
 const NUM_THREADS: usize = 6;
 
 fn main() {
+    let func = |x| x + 69;
     let mut data = Vec::new();
-    for i in 1..400_000_001_u32 {
+    for i in 1..500_000_001_i32 {
         data.push(i)
     }
-    let length = data.len();
-    let result = split_on_threads(data, |data| (data as f64).log(3.33333333));
-    println!("Size {}. Same: {}", result.len(), length == result.len());
+    let first = data.clone().into_iter().map(func).collect::<Vec<i32>>();
+    let second = split_on_threads(data, func);
+    assert!(first == second);
+    println!("{:?}", second.len());
 }
 
-fn split_on_threads<T, R>(data: Vec<T>, func: fn(t: T) -> R) -> Vec<R>
+fn split_on_threads<'a, T, R>(data: Vec<T>, func: fn(t: T) -> R) -> Vec<R>
 where
     T: 'static + Sync + Send + Clone,
     R: 'static + Sync + Send,
@@ -27,23 +28,16 @@ where
             result
         }
         len => {
-            let (send, rec) = mpsc::channel();
             let holder_size = len / NUM_THREADS;
-            for chunk in data.chunks(holder_size).map(|chunk| chunk.to_owned()) {
-                let thread_send = send.clone();
-                thread::spawn(move || {
-                    thread_send
-                        .send(chunk.into_iter().map(func).collect::<Vec<R>>())
-                        .unwrap()
-                });
-            }
-
+            let mut threads = Vec::new();
             let mut result = Vec::with_capacity(len);
-            for _ in 0..NUM_THREADS + 1 {
-                rec.recv()
-                    .unwrap()
-                    .into_iter()
-                    .for_each(|data| result.push(data));
+            for chunk in data.chunks(holder_size).map(|chunk| chunk.to_owned()) {
+                threads.push(thread::spawn(move || {
+                    chunk.into_iter().map(func).collect::<Vec<R>>()
+                }));
+            }
+            for thread in threads {
+                result.append(&mut thread.join().unwrap());
             }
             result
         }
