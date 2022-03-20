2f1576fb8be5c19a675ea47240183e7adf47c49b
Edward Zelis <zeledvards@gmail.com>
2022-03-20 04:34:31 +0200

    change type

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,7 @@ mod tests {
     use super::*;
     #[test]
     fn test_identity_of_data() {
-        let size = 500_000_000_i32;
+        let size = 500_000_000_u32;
         let mut data = Vec::with_capacity(size as usize);
         for i in 1..size + 1 {
             data.push(i)
